b7ba41371723ef6b2ec21b00ba1cc48e6155fa9a
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-04-13 01:45:50 -0400

    Update VideoCPU formatting

--- src/VideoCPU/main.c
+++ src/VideoCPU/main.c
@@ -1,7 +1,6 @@
 #include <complex.h>
 #include <math.h>
 #include <mpi.h>
-#include <png.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -19,170 +18,168 @@
 #define SIZE 1000
 #define OUTPUT_FILE_NAME "a.png"
 
+/**
+ *
+ * @brief Global variables.
+ *
+ */
 int x;
 int y;
-int width;
-int height;
-png_byte color_type;
-png_byte bit_depth;
-png_structp image;
-png_infop image_info;
 
-int number_of_passes;
-png_bytep *row_pointers;
-Frame frame;
-Block knapsack;
-png_byte **image_data;
+int WIDTH;
+int HEIGHT;
+int NUM_PASSES;
 
+png_byte COLOR_TYPE;
+png_byte BIT_DEPTH;
+png_byte **IMAGE_DATA;
+
+png_structp IMAGE;
+png_infop IMAGE_INFO;
+png_bytep *ROW_PTR;
+
+Frame FRAME;
+Block KNAPSACK;
+
+/**
+ *
+ * @brief Function prototypes.
+ *
+ */
 void write_image();
-void calculate(int);
+void calculate(int index);
 void allocate();
 void root_process();
-void child_process();
+void child_process(int rank, int processor_count);
 
 /**
  *
  * @brief Write the image to a file.
+ * @param void
+ * @return void
  *
  */
-void write_image()
-{
+void write_image() {
     char buffer[12];
-    snprintf(buffer, 12, "%d.png", frame.index);
+    snprintf(buffer, 12, "%d.png", FRAME.index);
     file = buffer;
-    color_type = PNG_COLOR_TYPE_RGBA;
-    bit_depth = 8;
-    number_of_passes = 1;
+    COLOR_TYPE = PNG_COLOR_TYPE_RGBA;
+    BIT_DEPTH = 8;
+    NUM_PASSES = 1;
 
     FILE *file = fopen(file, "wb");
-    if (!file)
-    {
-        printf("[write_png_file] File %s could not be opened for writing", file);
+    if (!file) {
+        fprintf(stderr, "[write_png_file] File %s could not be opened for writing", file);
         exit(1);
     }
 
-    image = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (!image)
-    {
-        printf("[write_png_file] png_create_write_struct failed");
+    IMAGE = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!IMAGE) {
+        fprintf(stderr, "[write_png_file] png_create_write_struct failed");
         exit(1);
     }
 
-    image_info = png_create_info_struct(image);
-    if (!image_info)
-    {
-        printf("[write_png_file] png_create_info_struct failed");
+    IMAGE_INFO = png_create_info_struct(image);
+    if (!IMAGE_INFO) {
+        fprintf(stderr, "[write_png_file] png_create_info_struct failed");
         exit(1);
     }
 
-    if (setjmp(png_jmpbuf(image)))
-    {
-        printf("[write_png_file] Error during init_io");
+    if (setjmp(png_jmpbuf(IMAGE))) {
+        fprintf(stderr, "[write_png_file] Error during init_io");
         exit(1);
     }
 
-    png_init_io(image, file);
-    if (setjmp(png_jmpbuf(image)))
-    {
-        printf("[write_png_file] Error during writing header");
+    png_init_io(IMAGE, file);
+    if (setjmp(png_jmpbuf(IMAGE))) {
+        fprintf(stderr, "[write_png_file] Error during writing header");
         exit(1);
     }
 
-    png_set_IHDR(image, image_info, width, height, bit_depth, color_type,
+    png_set_IHDR(IMAGE, IMAGE_INFO, WIDTH, HEIGHT, BIT_DEPTH, COLOR_TYPE,
                  PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-    png_write_info(image, image_info);
-    if (setjmp(png_jmpbuf(image)))
-    {
-        printf("[write_png_file] Error during writing bytes");
+
+    png_write_info(IMAGE, IMAGE_INFO);
+
+    if (setjmp(png_jmpbuf(IMAGE))) {
+        fprintf(stderr, "[write_png_file] Error during writing bytes");
         exit(1);
     }
 
-    png_set_compression_level(image, 6);
-    png_set_filter(image, 0, 0);
-    png_write_image(image, knapsack.data);
-    if (setjmp(png_jmpbuf(image)))
-    {
-        printf("[write_png_file] Error during end of write");
+    png_set_compression_level(IMAGE, 6);
+    png_set_filter(IMAGE, 0, 0);
+    png_write_image(IMAGE, KNAPSACK.data);
+
+    if (setjmp(png_jmpbuf(IMAGE))) {
+        fprintf(stderr, "[write_png_file] Error during end of write");
         exit(1);
     }
 
-    png_write_end(image, NULL);
+    png_write_end(IMAGE, NULL);
     fclose(file);
+
+    return;
 }
 
 /**
  *
  * @brief Calculate the mandelbrot set for a given frame.
  * @param index The index of the frame.
+ * @return void
  *
  */
-void calculate(int index)
-{
+void calculate(int index) {
     float scale = 1.5;
-    float cx = frame.real;
-    float cy = frame.imaginary;
-    float zx = scale * (float)(width / 2 - x) / (width / 2);
-    float zy = scale * (float)(height / 2 - y) / (height / 2);
+    float cx = FRAME.real;
+    float cy = FRAME.imaginary;
+    float zx = (scale * (float)(WIDTH / 2 - x) / (WIDTH / 2));
+    float zy = (scale * (float)(HEIGHT / 2 - y) / (HEIGHT / 2));
     int i = 0;
     int c = 0;
-    float tmp = zx * zx - zy * zy + cx;
+    float temp = (zx * zx - zy * zy + cx);
 
-    for (int y = index; y < height; y++, c++)
-    {
-        png_byte *row = knapsack.data[y];
-        for (int x = 0; x <= width; x++)
-        {
+    for (int y = index; y < HEIGHT; y++, c++) {
+        png_byte *row = KNAPSACK.data[y];
+        for (int x = 0; x <= WIDTH; x++) {
             png_byte *ptr = &(row[x * 4]);
             int bit = 0;
-            zx = scale * (float)(width / 2 - x) / (width / 2);
-            zy = scale * (float)(height / 2 - y) / (height / 2);
-            for (int i = 0; i < MAX_ITERATIONS; i++)
-            {
-                tmp = zx * zx - zy * zy + cx;
-                zy = 2.0 * zx * zy + cy;
-                zx = tmp;
-                if (zx * zx + zy * zy > 4.0)
-                {
+            zx = (scale * (float)(WIDTH / 2 - x) / (WIDTH / 2));
+            zy =( scale * (float)(HEIGHT / 2 - y) / (HEIGHT / 2));
+            for (int i = 0; i < MAX_ITERATIONS; i++) {
+                temp = (zx * zx - zy * zy + cx);
+                zy = (2.0 * zx * zy + cy);
+                zx = temp;
+                if (zx * zx + zy * zy > 4.0) {
                     bit = i % 5;
-                    if (bit == 0)
+                    if (bit == 0) {
                         bit = 5;
+                    }
 
                     break;
                 }
             }
 
-            if (bit == 0)
-            {
+            if (bit == 0) {
                 ptr[0] = 0;
                 ptr[1] = 0;
                 ptr[2] = 0;
-            }
-            else if (bit == 1)
-            {
+            } else if (bit == 1) {
                 ptr[0] = 204;
                 ptr[1] = 107;
                 ptr[2] = 73;
-            }
-            else if (bit == 2)
-            {
+            } else if (bit == 2) {
                 ptr[0] = 210;
                 ptr[1] = 162;
                 ptr[2] = 76;
-            }
-            else if (bit == 3)
-            {
+            } else if (bit == 3) {
                 ptr[0] = 236;
                 ptr[1] = 230;
                 ptr[2] = 194;
-            }
-            else if (bit == 4)
-            {
+            } else if (bit == 4) {
                 ptr[0] = 115;
                 ptr[1] = 189;
                 ptr[2] = 168;
-            }
-            else
-            {
+            } else {
                 ptr[0] = 153;
                 ptr[1] = 190;
                 ptr[2] = 183;
@@ -191,84 +188,100 @@ void calculate(int index)
             ptr[3] = 255;
         }
     }
+
+    return;
 }
 
 /**
  *
  * @brief Allocate memory for the image data.
+ * @param void
+ * @return void
  *
  */
-void allocate()
-{
-    image_data = (png_byte **)malloc(sizeof(png_byte *) * height);
-    for (int y = 0; y < height; y++)
-        image_data[y] = (png_byte *)malloc(sizeof(png_bytep) * width);
+void allocate() {
+    IMAGE_DATA = (png_byte **) malloc(sizeof(png_byte *) * HEIGHT);
+    for (int y = 0; y < HEIGHT; y++) {
+        IMAGE_DATA[y] = (png_byte *) malloc(sizeof(png_bytep) * WIDTH);
+    }
 
-    knapsack.height = height;
-    knapsack.width = width;
-    knapsack.size = height * width;
-    knapsack.place = 0;
-    knapsack.data = image_data;
+    KNAPSACK.height = HEIGHT;
+    KNAPSACK.width = WIDTH;
+    KNAPSACK.size = HEIGHT * WIDTH;
+    KNAPSACK.place = 0;
+    KNAPSACK.data = IMAGE_DATA;
+
+    return;
 }
 
 /**
  *
- * @brief Code ran by process rank 0 to handle results from all child processes,
+ * @brief Code ran by process rank 0 to handle results from all child processes.
+ * @param void
+ * @return void
  *
  */
-void root_process()
-{
-    bool wants_work = true;
-    for (int i = 0; i < FRAME_COUNT; i++)
-    {
-        frame.imaginary = 0.136 + (0.001 * i);
-        frame.index = i;
-        frame.real = -0.8 + (0.001 * i);
+void root_process() {
+    bool next = true;
 
-        MPI_Recv(&wants_work, 1, MPI_INT, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, &status);
-        printf("%d <<<\n", wants_work);
-        MPI_Send(&frame, 4, MPI_INT, wants_work, 1, MPI_COMM_WORLD);
+    for (int i = 0; i < FRAME_COUNT; i++) {
+        FRAME.imaginary = 0.136 + (0.001 * i);
+        FRAME.index = i;
+        FRAME.real = -0.8 + (0.001 * i);
+
+        MPI_Recv(&next, 1, MPI_INT, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, &status);
+        fprintf(stdout, "%d <<<\n", next);
+        MPI_Send(&FRAME, 4, MPI_INT, next, 1, MPI_COMM_WORLD);
     }
 
-    frame.index = -1;
-    for (int i = 1; i < processor_count; i++)
-        MPI_Send(&frame, 4, MPI_INT, i, 1, MPI_COMM_WORLD);
+    FRAME.index = -1;
+
+    for (int i = 1; i < processor_count; i++) {
+        MPI_Send(&FRAME, 4, MPI_INT, i, 1, MPI_COMM_WORLD);
+    }
+
+    return;
 }
 
 /**
  *
  * @brief Code ran by all child processes to handle work from the root process.
+ * @param rank The rank of the child process.
+ * @param processor_count The total number of processes.
+ * @return void
  *
  */
-void child_process(int rank, int processor_count)
-{
-    while (frame.index >= 0)
-    {
+void child_process(int rank, int processor_count) {
+    while (FRAME.index >= 0) {
         MPI_Send(&rank, 1, MPI_INT, 0, 3, MPI_COMM_WORLD);
-        MPI_Recv(&frame, 4, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        if (frame.index != -1)
-        {
+        MPI_Recv(&FRAME, 4, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        if (FRAME.index != -1) {
             allocate();
             calculate(0);
             write_image();
         }
     }
 
-    printf("Recieving %f - %d - %f \n", frame.imaginary, frame.index, frame.real);
-    for (int y = 0; y < height; y++)
-        free(knapsack.data[y]);
+    fprintf(stdout, "Recieving %f - %d - %f \n", FRAME.imaginary, FRAME.index, FRAME.real);
 
-    free(knapsack.data);
-    printf("Hello World from process %d of %d\n", rank, processor_count);
+    for (int y = 0; y < HEIGHT; y++) {
+        free(KNAPSACK.data[y]);
+    }
+
+    free(KNAPSACK.data);
+
+    return;
 }
 
 /**
  *
  * @brief Main function.
+ * @param argc The number of arguments.
+ * @param argv The arguments.
+ * @return 0
  *
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int rank;
     int processor_count;
     MPI_Status status;
@@ -277,19 +290,22 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &processor_count);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    width = 3840;
-    height = 2160;
-    frame.imaginary = 0.0;
-    frame.index = 0;
-    frame.real = 0.0;
+    WIDTH = 3840;
+    HEIGHT = 2160;
+
+    FRAME.imaginary = 0.0;
+    FRAME.index = 0;
+    FRAME.real = 0.0;
 
     allocate();
-    if (rank == 0)
-        root_process();
 
-    else
+    if (rank == 0) {
+        root_process();
+    } else {
         child_process(rank, processor_count);
+    }
 
     MPI_Finalize();
+
     return 0;
 }
