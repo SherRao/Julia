5166830c7ea21f60c3630e023763130df7db07bc
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-04-13 01:44:04 -0400

    Update block header formatting

--- src/LargePNG/block.h
+++ src/LargePNG/block.h
@@ -1,10 +1,16 @@
 #include <png.h>
 
+/**
+ * 
+ * @file block.h
+ * @brief Represents a block of data in the final video output.
+ * 
+ */
 typedef struct _Block {
-    int size;
-    int is_done;
-    int place;
-    int width;
-    int height;
-    png_byte **data;
+    int size;        // The size of the block in bytes
+    int is_done;     // Whether the block has been filled with data
+    int place;       // The place in the final video output
+    int width;       // The width of the block in pixels
+    int height;      // The height of the block in pixels
+    png_byte **data; // The data of the block
 } Block;
