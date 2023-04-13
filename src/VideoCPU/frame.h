58779549f14bb1396b59cd2454d2d1d128c461d5
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-04-13 01:45:28 -0400

    Update frame header formatting

--- src/VideoCPU/frame.h
+++ src/VideoCPU/frame.h
@@ -1,13 +1,11 @@
-/* Represents a frame in the final video output. */
-typedef struct _Frame
-{
-    /* The position of this frame*/
-    int index;
-
-    /* The real value of the point of this frame */
-    float real;
-
-    /* The imaginary value of the point of this frame */
-    float imaginary;
-
+/**
+ * 
+ * @file frame.h
+ * @brief Represents a frame in the video.
+ * 
+ */
+typedef struct _Frame {
+    int index;       // The position of this frame in the video.
+    float real;      // The real value of the point of this frame 
+    float imaginary; // The imaginary value of the point of this frame 
 } Frame;
