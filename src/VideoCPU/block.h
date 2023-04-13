6ea0724942537b7d4f8c73d1df4562ff21921642
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-04-13 01:45:09 -0400

    Update block header formatting

--- src/VideoCPU/block.h
+++ src/VideoCPU/block.h
@@ -1,21 +1,15 @@
 #include <png.h>
 
-/* Represents a block of data in the final video output. */
-typedef struct _Block
-{
-    /* The size of the block */
-    int size;
-
-    /* The position of the block */
-    int place;
-
-    /* The width of the block */
-    int width;
-
-    /* The height of the block */
-    int height;
-
-    /* The location of the image data of the block */
-    png_byte **data;
-
+/**
+ * 
+ * @file block.h
+ * @brief Represents a block of data in the final video output.
+ * 
+ */
+typedef struct _Block {
+    int size;        // The size of the block 
+    int place;       // The place of the block
+    int width;       // The width of the block
+    int height;      //  The height of the block 
+    png_byte **data; // The location of the image data of the block 
 } Block;
